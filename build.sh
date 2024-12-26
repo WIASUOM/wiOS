66fecb678a9dec08df368cca6d50489ae86b13df
WIASUOM <wiasuom@proton.me>
2024-12-26 15:08:06 +0400

    easier file system?

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
-#!/bin/sh
-set -e
-. ./headers.sh
-
-for PROJECT in $PROJECTS; do
-  (cd $PROJECT && DESTDIR="$SYSROOT" $MAKE install)
-done
+nasm -f elf32 boot.asm -o boot.o
+gcc -m32 -c kernel.c -o kernel.o
+gcc -m32 -c ./src/print.c -o ./src/print.o
+ld -m elf_i386 -T link.ld -o ./bin/kernel.bin boot.o kernel.o ./src/print.o
+qemu-system-x86_64 -kernel ./bin/kernel.bin
